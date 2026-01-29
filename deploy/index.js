a15a59a94b44a40ac3e08f31996cfe62bd06d564
mansi-okay <mansiagrawat2@gmail.com>
2026-01-29 23:14:53 +0530

    githubData added

--- deploy/index.js
+++ deploy/index.js
@@ -9,6 +9,42 @@ const app = express()
 // A port is like a door number on computer
 // const port = 4000
 
+const githubData = {
+  "login": "mansi-okay",
+  "id": 193275684,
+  "node_id": "U_kgDOC4UnJA",
+  "avatar_url": "https://avatars.githubusercontent.com/u/193275684?v=4",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/mansi-okay",
+  "html_url": "https://github.com/mansi-okay",
+  "followers_url": "https://api.github.com/users/mansi-okay/followers",
+  "following_url": "https://api.github.com/users/mansi-okay/following{/other_user}",
+  "gists_url": "https://api.github.com/users/mansi-okay/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/mansi-okay/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/mansi-okay/subscriptions",
+  "organizations_url": "https://api.github.com/users/mansi-okay/orgs",
+  "repos_url": "https://api.github.com/users/mansi-okay/repos",
+  "events_url": "https://api.github.com/users/mansi-okay/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/mansi-okay/received_events",
+  "type": "User",
+  "user_view_type": "public",
+  "site_admin": false,
+  "name": "Mansi Agrawat",
+  "company": null,
+  "blog": "",
+  "location": null,
+  "email": null,
+  "hireable": null,
+  "bio": null,
+  "twitter_username": null,
+  "public_repos": 4,
+  "public_gists": 0,
+  "followers": 1,
+  "following": 1,
+  "created_at": "2024-12-30T20:09:52Z",
+  "updated_at": "2026-01-16T14:12:05Z"
+}
+
 app.get('/', (request, response) => {
   response.send('Hello World!')
 })
@@ -25,6 +61,9 @@ app.get('/user', (req,res) => {
     res.send('<h2> YOUR PROFILE <h2>')
 })
 
+app.get('/github', (req,res) => {
+    res.json(githubData)
+})
 
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${process.env.PORT}`)
